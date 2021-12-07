516214c9a2a7ad6039a0145f86cd655e9f0e0a06
Clément LEFIEF <clement.lefief@gmail.com>
2021-12-07 15:34:14 +0100

    improvements by clement le best

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'article')]
+    #[Route('/home', name: 'home')]
     public function index(): Response
     {
         return $this->render('article/index.html.twig', [
